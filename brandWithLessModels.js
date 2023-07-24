639ad8e4736ddc22b95576ed9b970d285f7044a0
SrWillxD <willgra09@hotmail.com>
2023-07-24 19:44:58 -0300

    Adding method that returns the brand with the least number of models

---
+++ brandWithLessModels.js
@@ -0,0 +1,25 @@
+import captureJSON from "./captureJSON.js";
+
+function brandWithLessModels(){
+    const filePath = "./car-list.json";
+    const jsonData = captureJSON(filePath);
+
+    let modelCount = [];
+
+    jsonData.forEach(car => {
+        modelCount.push([car.brand, car.models.length]);
+    });
+
+    modelCount.sort((a, b) => a[1] - b[1]);
+
+    const fewerQuantityOfModels = modelCount[0][1];
+    const brandsTiedWithLessModels = [];
+
+    modelCount.forEach(item => {
+        if (item[1] === fewerQuantityOfModels) {
+            brandsTiedWithLessModels.push(item[0]);
+        }
+    });
+
+    return brandsTiedWithLessModels;
+}
