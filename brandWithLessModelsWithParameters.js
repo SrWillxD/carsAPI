c762c7a46c229bbaf49b22db2c9cc4fdca1e9951
SrWillxD <willgra09@hotmail.com>
2023-07-24 21:16:27 -0300

    Adding a method that returns the brands with the least number of models, and if there is a tie in the number of models, the alphabetical order of the models is considered as a tiebreaker.

---
+++ brandWithLessModelsWithParameters.js
@@ -0,0 +1,30 @@
+import captureJSON from "./captureJSON.js";
+
+function brandWithLessModelsWhitParameters(amount) {
+    const filePath = "./car-list.json";
+    const jsonData = captureJSON(filePath);
+
+    let modelCount = [];
+
+    jsonData.forEach(car => {
+        modelCount.push([car.brand, car.models.length]);
+    });
+
+    modelCount.sort((a, b) => {
+        if (b[1] !== a[1]) {
+            return a[1] - b[1];
+        }else{
+            return a[0].localeCompare(b[0]);
+        }
+    
+    });
+
+    const brandsTiedWithLessModels = [];
+    
+    for (let i = 0; i < amount; i++) {
+        brandsTiedWithLessModels.push(modelCount[i]);
+    }
+
+    return brandsTiedWithLessModels;
+}
+console.log(brandWithLessModelsWhitParameters(10));
