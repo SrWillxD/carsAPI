d82da0700fede70c79ba7698838faa55c0162f95
SrWillxD <willgra09@hotmail.com>
2023-07-25 08:06:23 -0300

    Adding a method that returns the models of a certain brand

---
+++ modelsOfACertainBrand.js
@@ -0,0 +1,16 @@
+import captureJSON from "./captureJSON.js";
+
+function modelsOfACertainBrand(brand){
+    const filePath = "./car-list.json";
+    const jsonData = captureJSON(filePath);
+
+    const modelList = [];
+
+    jsonData.forEach(car => {
+        if(car.brand.toLowerCase() === brand.toLowerCase()){
+            modelList.push(car.models);
+        }
+    });
+
+    return modelList;
+}
